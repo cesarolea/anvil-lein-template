be6d21028f92deb98356828a1c1f3d34d327d2b9
Cesar Olea <cesarolea@gmail.com>
2019-04-08 15:00:36 -0700

    Removed SNAPSHOT from version.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 All notable changes to this project will be documented in this file. This change log follows the conventions of [keepachangelog.com](http://keepachangelog.com/).
 
 ## [Unreleased]
+### Changed
+- Version is now 0.1.0. Removed SNAPSHOT for Clojars publication.
 
 ## 0.1.0 - 2019-04-08
 ### Added
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject anvil/lein-template "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject anvil/lein-template "0.1.0"
+  :description "A barebones leiningen template for ClojureScript with figwheel-main"
+  :url "https://github.com/cesarolea/anvil-lein-template"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :eval-in-leiningen true)
