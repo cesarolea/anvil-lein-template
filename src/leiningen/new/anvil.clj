1084ed44edd3f8555df8a63908e1c91179ce7901
Cesar Olea <cesarolea@gmail.com>
2019-04-08 22:33:30 -0700

    Support for template options.

--- src/leiningen/new/anvil.clj
+++ src/leiningen/new/anvil.clj
@@ -5,17 +5,26 @@
 (def render (renderer "anvil"))
 
 (defn anvil
-  "FIXME: write documentation"
-  [name]
-  (let [data {:name name
-              :sanitized (name-to-path name)}]
-    (main/info "Generating fresh 'lein new' anvil project.")
-    (->files data
-             ["src/clj/{{sanitized}}/core.clj" (render "core.clj" data)]
-             ["src/cljs/{{sanitized}}/core.cljs" (render "core.cljs" data)]
-             ["env/dev/clj/user.clj" (render "user.clj" data)]
-             ["figwheel-main.edn" (render "figwheel-main.edn" data)]
-             ["dev.cljs.edn" (render "dev.cljs.edn" data)]
-             ["prod.cljs.edn" (render "prod.cljs.edn" data)]
-             ["resources/public/index.html" (render "index.html" data)]
-             ["project.clj" (render "project.clj" data)])))
+  "Creates a ClojureScript project with optional Docker support"
+  ([name & opts]
+   (main/info "Generating a new anvil project")
+   (let [data {:name name
+               :sanitized (name-to-path name)}
+         opts (into #{} opts)
+         base [["src/clj/{{sanitized}}/core.clj" (render "core.clj" data)]
+               ["src/cljs/{{sanitized}}/core.cljs" (render "core.cljs" data)]
+               ["env/dev/clj/user.clj" (render "user.clj" data)]
+               ["figwheel-main.edn" (render "figwheel-main.edn" data)]
+               ["dev.cljs.edn" (render "dev.cljs.edn" data)]
+               ["prod.cljs.edn" (render "prod.cljs.edn" data)]
+               ["resources/public/index.html" (render "index.html" data)]
+               ["project.clj" (render "project.clj" data)]]
+         docker [["Dockerfile" (render "Dockerfile" data)]
+                 ["docker-compose.yml" (render "docker-compose.yml" data)]]]
+
+     (apply ->files data
+            (cond
+              (contains? opts "+docker") (concat base docker)
+              :else base))))
+  ([name]
+   (anvil name nil)))
