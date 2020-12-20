28ed2caecb322cd663b063e989ebb82e097bd315
Cesar Olea <cesarolea@gmail.com>
2020-12-20 13:45:12 -0700

    Bump version

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject anvil/lein-template "0.2.4"
+(defproject anvil/lein-template "0.3.0"
   :description "A barebones leiningen template for ClojureScript with figwheel-main"
   :url "https://github.com/cesarolea/anvil-lein-template"
-  :deploy-repositories [["clojars" {:url "https://repo.clojars.org"
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo/"
                                     :username :env/clojars_username
                                     :password :env/clojars_password
-                                    :sign-releases false}]]
+                                    :sign-releases true}]]
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :eval-in-leiningen true)
